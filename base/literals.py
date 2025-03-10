3ecb33e776edc6cbc26a40ea33a6b11a124e4283
Gennadiy <renardes@gmail.com>
2025-03-10 19:03:24 +0500

    основные литералы встроенных типов

---
+++ base/literals.py
@@ -0,0 +1,62 @@
+# NoneType
+None
+
+
+# int
+1234
+
+
+# float
+1.2
+.935
+
+
+# complex
+2j
+10-5j
+
+
+# bool
+True
+False
+
+
+# str
+'он сказал: "сегодня не получится"'
+
+"he can't do it"
+
+'''первая строчка текста
+вторая строчка текста
+третья строчка текста'''
+
+"""The following sections describe the standard types that are built into the interpreter.
+
+The principal built-in types are numerics, sequences, mappings, classes, instances and exceptions.
+
+Some collection classes are mutable. The methods that add, subtract, or rearrange their members in place, and don’t return a specific item, never return the collection instance itself but None."""
+
+r'd:\data\networks'
+
+
+# list
+[]
+[67]
+[10, 'abc', [.1, .2]]
+
+
+# tuple
+()
+(11,)
+(5, 6, 7)
+'a', 'b', 'c'
+
+
+# set
+{'abc'}
+{1, 'ab', (.1, .1)}
+
+
+# dict
+{1: 'a', 2: 'v'}
+
