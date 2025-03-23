4eb5c2d5cefc3baa7a2b96257cf11cdc110c53c3
Gennadiy <renardes@gmail.com>
2025-03-23 21:55:38 +0500

    логические операторы

---
+++ base/operators3.py
@@ -0,0 +1,40 @@
+# >>> not True
+# False
+# >>>
+# >>> not False
+# True
+
+
+# >>> False or False
+# False
+# >>> True or False
+# True
+# >>> False or True
+# True
+# >>> True or True
+# True
+
+
+# >>> False and False
+# False
+# >>> True and False
+# False
+# >>> False and True
+# False
+# >>> True and True
+# True
+
+
+# >>> True and False and True
+# False
+# >>>
+# >>> True and False or True
+# True
+# >>>
+# >>> True and (False or True)
+# True
+
+
+# >>> 5 > 1 and 1 > 0
+# True
+
