f9d6f62aed79d7062b4acb120f7f10be76b9bc30
Gennadiy <renardes@gmail.com>
2025-03-23 21:55:39 +0500

    инструкция перехвата исключений try...except...else...finally

---
+++ base/exceptions1.py
@@ -0,0 +1,51 @@
+# try:
+#     <блок_1>
+# 
+# except (<тип_исключения_1>, <тип_исключения_2>):
+#     <блок_2>
+# 
+# except <тип_исключения_3> as exception_object:
+#     <блок_3>
+# 
+# except <тип_исключения_4>:
+#     <блок_4>
+# 
+# ...
+# 
+# except:
+#     <блок_5>
+# 
+# else:
+#     <блок_6>
+# 
+# finally:
+#     <блок_7>
+
+
+try:
+    pass
+except:
+    pass
+
+
+try:
+    pass
+except:
+    pass
+else:
+    pass
+
+
+try:
+    pass
+except:
+    pass
+finally:
+    pass
+
+
+try:
+    pass
+finally:
+    pass
+
