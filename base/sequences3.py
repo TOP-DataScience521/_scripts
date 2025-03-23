79f24f9e923b473a85c8d30e0a8282cbc0af982f
Gennadiy <renardes@gmail.com>
2025-03-23 21:55:43 +0500

    отличия между типом str и другими типами последовательностей при обращении по единичному индексу и при взятии срезов

---
+++ base/sequences3.py
@@ -0,0 +1,35 @@
+numbers = [9, 3, -1, 12, 10, 7, -5]
+
+# >>> numbers[0]
+# 9
+# >>> type(numbers[0])
+# <class 'int'>
+# >>>
+# >>> numbers[0:1]
+# [9]
+# >>> type(numbers[0:1])
+# <class 'list'>
+# >>>
+# >>> numbers[0:2]
+# [9, 3]
+# >>> type(numbers[0:2])
+# <class 'list'>
+
+
+word = 'слово'
+
+# >>> word[0]
+# 'с'
+# >>> type(word[0])
+# <class 'str'>
+# >>>
+# >>> word[0:1]
+# 'с'
+# >>> type(word[0:1])
+# <class 'str'>
+# >>>
+# >>> word[0:2]
+# 'сл'
+# >>> type(word[0:2])
+# <class 'str'>
+
