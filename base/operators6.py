58b977060ad7660dc6897f521a4ad89450654cf2
Gennadiy <renardes@gmail.com>
2025-03-23 21:55:44 +0500

    оператор проверки на принадлежность

---
+++ base/operators6.py
@@ -0,0 +1,29 @@
+# >>> 5 in range(10)
+# True
+# >>>
+# >>> 10 in range(10)
+# False
+
+
+# >>> 'd' in 'abcd'
+# True
+# >>>
+# >>> 'z' in 'abcd'
+# False
+# >>>
+# >>> 'cd' in 'abcd'
+# True
+
+
+# >>> 5 in [1, 2, 3]
+# False
+# >>>
+# >>> 2 in [1, 2, 3]
+# True
+# >>>
+# >>> [2, 3] in [1, 2, 3]
+# False
+# >>>
+# >>> [2, 3] in [1, 2, 3, [2, 3]]
+# True
+
