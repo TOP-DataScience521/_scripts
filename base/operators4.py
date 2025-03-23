92615b42289093deda9dde1f8de277f9d030d574
Gennadiy <renardes@gmail.com>
2025-03-23 21:55:38 +0500

    использование логических операторов с объектами разных типов

---
+++ base/operators4.py
@@ -0,0 +1,38 @@
+# >>> 5 and 10
+# 10
+# >>> 0 and 10
+# 0
+# >>> 7 and 0
+# 0
+# >>> 1 and 0 and -1
+# 0
+# >>> 1 and 0 and -1 and ''
+# 0
+
+
+# >>> 5 or 9
+# 5
+# >>> 0 or 9
+# 9
+# >>> 5 or 0
+# 5
+# >>> -1 or 0 or 1
+# -1
+# >>> 2 or -1 or 0 or 1
+# 2
+# >>> 0 or ''
+# ''
+
+
+# >>> name = input() or 'USERNAME'
+# GennDALF
+# >>>
+# >>> name
+# 'GennDALF'
+
+# >>> name = input() or 'USERNAME'
+# 
+# >>>
+# >>> name
+# 'USERNAME'
+
