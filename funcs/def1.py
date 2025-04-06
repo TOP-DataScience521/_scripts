32d9864f0e47b9440e18e039cc77a0ff9e403f9c
Gennadiy <renardes@gmail.com>
2025-04-06 20:23:01 +0500

    общий вид объявления функции

---
+++ funcs/def1.py
@@ -0,0 +1,21 @@
+# def <имя_функции>(<параметры_функции>):
+#     """<документация_функции>"""
+#     <тело_функции>
+#     ...
+
+
+def greeting():
+    print('hello world')
+
+
+# объект функции
+# >>> greeting
+# <function greeting at 0x000002356E1BA200>
+# >>>
+# >>> type(greeting)
+# <class 'function'>
+
+# вызов функции
+# >>> greeting()
+# hello world
+
