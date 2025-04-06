23adeafffc9b173d6241a22f8b76f878a8f41c04
Gennadiy <renardes@gmail.com>
2025-04-06 20:23:04 +0500

    строго ключевые параметры и аргументы

---
+++ funcs/def5.py
@@ -0,0 +1,13 @@
+def track_to_db(*, title, artist, album, channels, bit_depth, sample_rate):
+    pass
+
+
+track_to_db(
+    title='Boom', 
+    artist='Boomers', 
+    album='BOOM!', 
+    channels=6, 
+    bit_depth=8, 
+    sample_rate=44.1
+)
+
