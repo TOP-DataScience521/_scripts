a9a60f7fd7fac80d742e7deaa21f33a2ec62c472
Gennadiy <renardes@gmail.com>
2025-04-27 19:28:00 +0500

    виды путей

---
+++ base/paths1.py
@@ -0,0 +1,22 @@
+# абсолютные пути
+
+# nt
+r'c:\file.ext'
+r'd:\dir\file.ext'
+
+# posix
+'/home/user/file'
+'/usr/program'
+
+
+# относительные пути
+
+# nt
+r'scripts\funcs'
+r'data\rel\models.db'
+r'converter.py'
+
+# posix
+'tmp/001/mount.c'
+'hosts'
+
