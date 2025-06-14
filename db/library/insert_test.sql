ee9ec2bec4c19638323ec282cdae16fb747d34f0
Gennadiy <renardes@gmail.com>
2025-06-14 15:45:06 +0500

    запросы добавления данных

---
+++ db/library/insert_test.sql
@@ -0,0 +1,35 @@
+delete from authors;
+
+insert into authors values (1, "Толстой", "Лев");
+
+insert   into 
+    authors     values 
+  
+        (2,     "Достоевский", 
+ "Фёдор"
+)
+            ;
+
+insert into authors
+  (first_name, last_name)
+values
+  ("Рэй", "Брэдбери"),
+  ("Лоис", "Буджолд"),
+  ("Мария", "Семёнова");
+
+insert into authors
+  (first_name, last_name)
+values
+  ("Николай", "Гоголь"),
+  ("Александр", "Пушкин"),
+  ("Эрих", "Ремарк"),
+  ("Уильям", "Шекспир"),
+  ("Гомер", "");
+
+
+insert into books
+  (title, author_id)
+values
+  (),
+  ();
+
