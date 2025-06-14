e6a289fc1598987d9ecc9c7d396d749d35c6d173
Gennadiy <renardes@gmail.com>
2025-06-14 15:45:07 +0500

    запросы изменения данных

---
+++ db/library/update.sql
@@ -0,0 +1,11 @@
+update authors
+  set last_name = "<ФАМИЛИЯ>";
+
+update authors
+  set last_name = "Гоголь"
+  where id = 17;
+
+update authors
+  set last_name = "<ФАМИЛИЯ>"
+  where last_name = "";
+
