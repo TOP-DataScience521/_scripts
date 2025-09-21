f00eb49ce2a25ad56db4df4cc40df3fdb435933d
Gennadiy <renardes@gmail.com>
2025-09-21 16:06:16 +0500

    замена процедурного кода классом

--- math/hypothesis1.py
+++ math/hypothesis1.py
@@ -1,78 +1,66 @@
-from matplotlib import pyplot as plt
 from numpy import array, sort
 
-'αχ²σ'
+
+class RCrit:
+    """
+    Класс для проверки принадлежности элементов ряда к нормально распределённой генеральной совокупности.
+    """
+    def __init__(self, data, significance, outliers=1, crit_source='std'):
+        self.data = array(data)
+        self.n = len(data)
+        self.significance = significance
+        self._iterations = outliers
+        if crit_source == 'std':
+            self.crit = float(input('граница крититческой области r-критерия: '))
+        else:
+            ...
+        self.H0: dict[float, bool] = {}
+    
+    def __bool__(self):
+        return bool(self.H0)
+    
+    def __repr__(self):
+        try:
+            el1, el2, el3 = self.data[:3]
+        except ValueError:
+            el1, el2, el3 = '', '', ''
+        return f'<RCrit: [{el1} {el2} {el3} ...], significance={self.significance:.3f}>'
+    
+    def _observed_criteria(self, element):
+        return abs(self.data.mean() - element) / (self.data.var() * ((self.n-1)/self.n)**0.5)
+    
+    def check(self, element):
+        r_obs = self._observed_criteria(element)
+        H0 = r_obs < self.crit
+        self.H0[element] = H0
+        return H0, r_obs
+    
+    def check_all_outliers(self):
+        print(
+            f'\nпроверка гипотезы о приндлежности значений из ряда N к генеральной совокупности',
+            f'уровень значимости: {self.significance:.2f}',
+            f'число степеней свободы: {self.n - 2}',
+            sep='\n'
+        )
+        data = sort(self.data)
+        for _ in range(self._iterations):
+            for i in (0, -1):
+                element = data[i]
+                H0, r_obs = self.check(element)
+                print(
+                    f'\nзначение: {element}',
+                    f'{r_obs:.3f} {"<" if H0 else ">="} {self.crit:.3f}',
+                    f'{element} {"" if H0 else "не "}принадлежит генеральной совокупности ({"не " if H0 else ""}является промахом)',
+                    sep='\n'
+                )
+            data = data[1:-1]
+        return self.H0
 
 
-N = array([227, 258, 271, 288, 292, 301, 322, 331, 372, 413])
-R = array([371, 379, 388, 396, 395, 406, 460, 408, 409, 411, 419, 424, 441, 397])
-B = array([312, 329, 248, 321, 418, 267, 193, 375, 358])
-
-α = 0.05
-
-
-n = len(N)
-r_obs = abs(N.mean() - N.min()) / (N.var() * ((n-1)/n)**0.5)
-print(
-    f'\nпроверка гипотезы о приндлежности значения {N.min()} из ряда N к генеральной совокупности',
-    f'уровень значимости: {α:.2f}',
-    f'число степеней свободы: {len(N) - 2}',
-    f'наблюдаемое значение r-критерия: {r_obs:.3f}',
-    sep='\n'
-)
-r_crit = float(input('граница крититческой области r-критерия: '))
-H0 = r_obs < r_crit
-print(
-    f'{r_obs:.3f} {"<" if H0 else ">="} {r_crit:.3f}',
-    f'{N.min()} {"" if H0 else "не "}принадлежит генеральной совокупности ({"не " if H0 else ""}является промахом)',
-    sep='\n'
-)
-r_obs = abs(N.mean() - N.max()) / (N.var() * ((n-1)/n)**0.5)
-print(
-    f'\nпроверка гипотезы о приндлежности значения {N.max()} из ряда N к генеральной совокупности',
-    f'уровень значимости: {α:.2f}',
-    f'число степеней свободы: {len(N) - 2}',
-    f'наблюдаемое значение r-критерия: {r_obs:.3f}',
-    f'граница крититческой области r-критерия: {r_crit:.3f}',
-    sep='\n'
-)
-H0 = r_obs < r_crit
-print(
-    f'{r_obs:.3f} {"<" if H0 else ">="} {r_crit:.3f}',
-    f'{N.max()} {"" if H0 else "не "}принадлежит генеральной совокупности ({"не " if H0 else ""}является промахом)',
-    sep='\n'
-)
-
-
-n = len(R)
-r_obs = abs(R.mean() - R.min()) / (R.var() * ((n-1)/n)**0.5)
-print(
-    f'\nпроверка гипотезы о приндлежности значения {R.min()} из ряда R к генеральной совокупности',
-    f'уровень значимости: {α:.2f}',
-    f'число степеней свободы: {len(R) - 2}',
-    f'наблюдаемое значение r-критерия: {r_obs:.3f}',
-    sep='\n'
-)
-r_crit = float(input('граница крититческой области r-критерия: '))
-H0 = r_obs < r_crit
-print(
-    f'{r_obs:.3f} {"<" if H0 else ">="} {r_crit:.3f}',
-    f'{R.min()} {"" if H0 else "не "}принадлежит генеральной совокупности ({"не " if H0 else ""}является промахом)',
-    sep='\n'
-)
-r_obs = abs(R.mean() - R.max()) / (R.var() * ((n-1)/n)**0.5)
-print(
-    f'\nпроверка гипотезы о приндлежности значения {R.max()} из ряда R к генеральной совокупности',
-    f'уровень значимости: {α:.2f}',
-    f'число степеней свободы: {len(R) - 2}',
-    f'наблюдаемое значение r-критерия: {r_obs:.3f}',
-    f'граница крититческой области r-критерия: {r_crit:.3f}',
-    sep='\n'
-)
-H0 = r_obs < r_crit
-print(
-    f'{r_obs:.3f} {"<" if H0 else ">="} {r_crit:.3f}',
-    f'{R.max()} {"" if H0 else "не "}принадлежит генеральной совокупности ({"не " if H0 else ""}является промахом)',
-    sep='\n'
-)
+if __name__ == '__main__':
+    
+    α = 0.05
+    
+    N = RCrit([227, 258, 271, 288, 292, 301, 322, 331, 372, 413], α)
+    R = RCrit([371, 379, 388, 396, 395, 406, 460, 408, 409, 411, 419, 424, 441, 397], α, 2)
 
